6e089598b728713c6f1d24500116780b7db16b90
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2025-12-29 09:25:13 +0800

    added functions throttle debounce

--- scripts/funcs.utils.js
+++ scripts/funcs.utils.js
@@ -16,3 +16,31 @@ export function selectionDropdown() {
    document.querySelector(".container-selection").classList.toggle("show");
    document.querySelector(".dropdown-btn").classList.toggle("show");
 }
+
+function throttle(func, ms) {
+   let isWaiting = false;
+
+   return function (event) {
+      if (isWaiting) return;
+      isWaiting = true;
+      func.call(event, this);
+      setTimeout(() => {
+         func.call(event, this);
+         isWaiting = false;
+      }, ms);
+   };
+}
+
+function debounce(func, ms) {
+   let reference;
+   return function (event) {
+      clearTimeout(reference);
+      reference = setTimeout(() => {
+         func.call(event, this);
+      }, ms);
+   };
+}
+
+export {
+   
+}
