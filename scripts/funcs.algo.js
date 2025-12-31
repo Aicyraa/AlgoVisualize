a83685c979c4fd65a11f784eb959eaef008247d9
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2025-12-31 20:23:19 +0800

    new func sortCompare - for adding a class to the current index sortSwap - for adding animation and swapping values in graph container bubble adjusted) - for sortCompare compatibility

--- scripts/funcs.algo.js
+++ scripts/funcs.algo.js
@@ -1,32 +1,31 @@
 import { sleep } from "./funcs.utils.js";
 
 async function bubble(arr) {
-   for (let i = 0; i < arr.length - 1; i++) {
-      for (let j = 0; j < arr.length - i - 1; j++) {
-         console.log("comparing");
-         sortCompare(j, j + 1);
-         await sleep(2000);
+   let sorted = [];
+   let n = arr.length;
+   
+   for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+         
+         sortCompare([], j, j + 1);
+         await sleep(1000);
 
          if (arr[j] > arr[j + 1]) {
+            // sortSwap(j, j + 1);
             let temp = arr[j];
-            sortSwap(j, j + 1);
             arr[j] = arr[j + 1];
             arr[j + 1] = temp;
          }
       }
+      sorted.push(n - i - 1);
+      sortCompare(sorted);
    }
-   console.log(arr);
-
    return arr;
 }
 
 function selection(arr) {
    let n = arr.length;
-   console.log(n);
-
    for (let i = 0; i < n - 1; i++) {
-      console.log(arr[i]);
-
       let smallest = arr[i];
       let index = 0;
       for (let j = i; j < n; j++) {
@@ -35,7 +34,6 @@ function selection(arr) {
             smallest = arr[j];
          }
       }
-
       arr[index] = arr[i];
       arr[i] = smallest;
    }
@@ -45,28 +43,33 @@ function selection(arr) {
 
 // comparing, swapping
 
-function sortCompare(index1, index2, special) {
+function sortCompare(sorted = [], idxA, idxB, idxSpecial) {
    /* 
       1. get the index of the correct value
       2. apply css class
       3. remove the class after comparing
    */
-
    const points = document.querySelectorAll("#bar");
-   function addStyle() {
-      points[index1].classList.toggle("current");
-      points[index2].classList.toggle("current");
+
+   function styleAdd() {
+      points.forEach((point, index) => {
+         if (sorted.includes(index)) {
+            point.classList.add("sorted");
+         } else if (index == idxA || index == idxB) {
+            point.classList.add("current");
+         }
+      });
    }
 
-   addStyle();
-   setTimeout(addStyle, 2000);
-
-   if (special) {
-      points[special].classList.add("correct-value");
+   function styleRemove() {
+      points.forEach((point) => point.classList.remove("current"));
    }
+
+   styleAdd();
+   setTimeout(styleRemove, 1000);
 }
 
-function sortSwap(index1, index2) {
+function sortSwap(idxA, idxB) {
    // accepts 2 values which is the index of a value in an array, and swap them while animation is playing
 }
 
