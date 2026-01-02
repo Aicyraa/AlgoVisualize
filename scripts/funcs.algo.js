cdebd8e191c6cc5963ff03bb676709e2faafb8c0
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-01-02 22:40:08 +0800

    ...

--- scripts/funcs.algo.js
+++ scripts/funcs.algo.js
@@ -22,37 +22,43 @@ async function bubble(arr) {
       sorted.push(n - i - 1);
       sortCompare(sorted);
    }
-   return arr;
 }
 
 async function selection(arr) {
+   // first element always has the "current" class
+   // hindi tama ung pag swap bandang gitna
+   // issue
+   const last = n - i - 1;
    let sorted = [];
    let n = arr.length;
-   for (let i = 0; i < n - 1; i++) {
-      let smallest = arr[i];
-      let index = 0;
-      for (let j = i; j < n; j++) {
-         sortCompare([], false, i, index);
-         await sleep(sortCompareTime);
-         if (smallest >= arr[j]) {
-            index = j;
-            smallest = arr[j];
+
+   for (let i = 0; i < n; i++) {
+      let bigIdx = 0;
+      for (let j = 0; j < n - i; j++) {
+         sortCompare([], j, undefined, index);
+         await sleep(sortCompareTime + 1000);
+
+         if (arr[j] >= arr[bigIdx]) {
+            bigIdx = j;
          }
       }
-      arr[index] = arr[i];
-      arr[i] = smallest;
-      // sortCompare([], index, i)
 
-      sorted.push(i);
+      // swapping the bigIdx to last and vice versa
+      sortSwap(bigIdx, last);
+      arr[bigIdx] = arr[last];
+      arr[last] = arr[bigIdx];
+
+      // pushing the last index to the sorted array
+      sorted.push(n - i - 1);
       sortCompare(sorted);
+      await sleep(sortCompareTime - 500);
    }
-
+   console.log(arr);
    return arr;
 }
 
 function insertion(arr) {
    let n = arr.length;
-   [2, 2, 5, 3, 7, 1];
    for (let i = 0; i < n; i++) {
       let temp = arr[i];
       let j = i;
@@ -65,68 +71,84 @@ function insertion(arr) {
    return arr;
 }
 
+function merge(arr) {
+   // divides the array into N sublist, where the sublist must contain only 1 element
+   // takes the 2 singleton array and merge them
+   // consider the first element of both 2 sublist
+   // the element who has the first lesser value become the new element
+}
+
 // comparing, swapping
 
 function sortCompare(sorted = [], idxA, idxB, idxSpecial) {
    const points = document.querySelectorAll("#bar");
    function styleAdd() {
+      // check which index is provided, and apply the corresponding class
       points.forEach((point, index) => {
          if (sorted.includes(index)) {
             point.classList.add("sorted");
+         } else if (idxSpecial == index) {
+            // correct-value must stay until there is another biggest value
+            point.classList.add("correct-value");
          } else if (index == idxA || index == idxB) {
             point.classList.add("current");
-         } else if (idxSpecial == index) {
-            point.classList.add("correct-value");
          }
       });
    }
-   function styleRemove() {
+
+   // remove the class "current" each call
+   function styleCurrentRemove() {
       points.forEach((point) => {
          point.classList.remove("current");
       });
    }
+
+   // remove the class "correct-value"
+   // but this should be remove only after another correct value is provided
+   function styleSpecialRemove() {
+      points.forEach((point) => {
+         point.classList.remove("correct-value");
+      });
+   }
+
+   // style add
    styleAdd();
-   setTimeout(styleRemove, sortCompareTime);
+   // currect class remove after sortCompareTime
+   setTimeout(styleCurrentRemove, sortCompareTime);
+   // currect class remove after sortCompareTime
+   setTimeout(styleSpecialRemove, sortCompareTime);
 }
 
 function sortSwap(idxA, idxB) {
-   /* 
-   1. Record current positions of bars (getBoundingClientRect)
-   2. Swap the bars in theDOM instantly
-   3. Measure new positions   
-   4. Apply a transform: translateX(...) to visually move them back
-   5. Let CSS transition move them into place
-   */
    const points = document.querySelectorAll(".points");
 
    let elmtA, elmtB, elmtParent;
    let elmtA_Before, elmtA_After;
    let elmtB_Before, elmtB_After;
 
+   // for easy logic, the first index must be the first element
    if (idxA === idxB) return;
    else if (idxA > idxB) [idxA, idxB] = [idxB, idxA];
+
    elmtA = points[idxA];
    elmtB = points[idxB];
    elmtParent = elmtA.parentNode;
 
+   // for swapping element
    function swap() {
       elmtParent.insertBefore(elmtA, elmtB.nextSibling);
       elmtParent.insertBefore(elmtB, elmtA);
    }
 
+   // for getting the before and after position
    function position() {
       const A = elmtA.getBoundingClientRect();
       const B = elmtB.getBoundingClientRect();
       return [A.left, B.left];
    }
 
-   // inverse transform
-   // checks if A > B
-   // if true; A shoud inverse to left side
-   // otherwise A should inverse to right side
+   // applying the visual animation after the script 
    function animate() {
-      // debugger
-
       elmtA.style.transform = `translateX(${elmtA_Before - elmtA_After}px)`;
       elmtB.style.transform = `translateX(${elmtB_Before - elmtB_After}px)`;
       setTimeout(() => {
